8454352fbb10a09be2f741e5547da430233b713f
s1ddiq <siddiq.nasheed@gmail.com>
2025-02-10 20:30:35 -0500

    redirect user to sign-in (if no session was found)

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -3,27 +3,32 @@
 import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const createSessionClient = async () => {
-    const client = new Client()
-        .setEndpoint(appwriteConfig.endpointUrl)
-        .setProject(appwriteConfig.projectId);
-
-    const session = (await cookies()).get("appwrite-session");
-
-    // console.log("Retrieved session cookie:", session);
-
-    if (!session || !session.value) throw new Error("No session");
-
-    client.setSession(session.value);
-    return {
-        get account() {
-            return new Account(client);
-        },
-        get databases() {
-            return new Databases(client);
-        },
-    };
+    try {
+        const client = new Client()
+            .setEndpoint(appwriteConfig.endpointUrl)
+            .setProject(appwriteConfig.projectId);
+    
+        const session = (await cookies()).get("appwrite-session");
+    
+        // console.log("Retrieved session cookie:", session);
+    
+        if (!session || !session.value) return redirect('/sign-in')
+    
+        client.setSession(session.value);
+        return {
+            get account() {
+                return new Account(client);
+            },
+            get databases() {
+                return new Databases(client);
+            },
+        };
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const createAdminClient = async () => {
