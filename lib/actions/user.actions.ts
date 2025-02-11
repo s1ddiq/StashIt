92c7239eff12cf4cc4d441d81d74c5ee5ddc4fba
s1ddiq <siddiq.nasheed@gmail.com>
2025-02-10 20:33:53 -0500

    turn getCurrentUser into try-catch block

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -81,18 +81,22 @@ export const verifySecret = async ({accountId, password}: {accountId: string, pa
 
 
 export const getCurrentUser = async () => {
-    const {databases, account} = await createSessionClient();
-    const result = await account.get();
-
-    const user = await databases.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.usersCollectionId,
-        [Query.equal("accountId", result.$id)]
-    )
-
-
-    if(user.total <= 0) return null;
-    return parseStringify(user.documents[0]);
+    try {     
+        const {databases, account} = await createSessionClient();
+        const result = await account.get();
+    
+        const user = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.usersCollectionId,
+            [Query.equal("accountId", result.$id)]
+        )
+    
+    
+        if(user.total <= 0) return null;
+        return parseStringify(user.documents[0]);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export const signOutUser = async () => {
